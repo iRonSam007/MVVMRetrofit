619c836d479f32aa1997f9894b40e004ca46ddb1
iRonSam007 <abida.invest10@gmail.com>
2024-10-21 00:34:31 +0200

    feat: add view, RV adapter

--- app/src/main/java/com/example/mvvmretrofit/MainActivity.kt
+++ app/src/main/java/com/example/mvvmretrofit/MainActivity.kt
@@ -1,19 +1,42 @@
 package com.example.mvvmretrofit
 
+import UserAdapter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mvvmretrofit.databinding.ActivityMainBinding
 
 const val TAG = "MVVMRetroApp"
 
-//Observe the livedata from ViewModel and update UI
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val userViewModel: UserViewModel by viewModels()
+    private lateinit var userAdapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Setup RecyclerView
+        userAdapter = UserAdapter()
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = userAdapter
+
+        // Observe the LiveData from ViewModel
+        userViewModel.getUsers().observe(this, Observer { users ->
+            users?.let {
+                userAdapter.submitList(it)
+            }
+        })
     }
 }
-
--- app/src/main/java/com/example/mvvmretrofit/UserViewModel.kt
+++ app/src/main/java/com/example/mvvmretrofit/UserViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.mvvmretrofit
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 
 //Communicate with repo and expose data to UI through LiveData
-class UserViewModel {
+class UserViewModel: ViewModel() {
     private val userRepository = UserRepository()
 
     fun getUsers(): LiveData<List<User>> {
