ef2cccf10a65b132e0cc09fc156591fee3790db0
iRonSam007 <abida.invest10@gmail.com>
2024-10-20 22:20:11 +0200

    feat: add api, repository and viewmodel implementation

--- app/src/main/java/com/example/mvvmretrofit/UserViewModel.kt
+++ app/src/main/java/com/example/mvvmretrofit/UserViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.mvvmretrofit
 
+import androidx.lifecycle.LiveData
+
 //Communicate with repo and expose data to UI through LiveData
 class UserViewModel {
+    private val userRepository = UserRepository()
 
+    fun getUsers(): LiveData<List<User>> {
+        return userRepository.getUsers()
+    }
 }
